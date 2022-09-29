bf379b163c59671889b306150fa48a5d4c6a8eeb
Francois Cepegra <titeca.f@gmail.com>
2022-09-29 16:26:03 +0200

    ajout d'un titre qui n'est pas stilay

--- src/components/Accueil.tsx
+++ src/components/Accueil.tsx
@@ -2,6 +2,7 @@ const Accueil = () => {
   return (
     <section className="flex flex-col justify-between h-screen">
       <button className="btn btn-block py-2 rounded-none btn-primary"><i className="fa-sharp fa-solid fa-download px-4"></i> Installer l'application</button>
+      <h1>je suis un logoooo</h1>
     <section className="flex flex-col my-0 mx-auto w-4/5">
       <button className="btn my-4">Scanner QR</button>
       <button className="btn my-4">Se connecter</button>
