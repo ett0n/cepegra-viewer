7ce78d4a1af2b91883371b4e193f2ea1b5dbd6d5
Francois Cepegra <titeca.f@gmail.com>
2022-10-03 11:46:17 +0200

    router poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "aframe";
 import ComposantExample from './components/ComposantExample'
 import Accueil from './components/Accueil'
 
-function App() {
+
+function App(): JSX.Element {
+  const [location, setLocation] = useState('ComposantExample');
+  const pages = ['ComposantExample', 'Accueil'];
+
+  useEffect( () => {
+    const path = window.location.pathname.replace('/', '')
+
+    if (pages.includes(path)) {
+      setLocation(path);
+    } else {
+      setLocation("404");
+    }
+  }, []);
+  const handleNavClick = (ev: React.MouseEvent<HTMLAnchorElement>) => {
+    ev.preventDefault();
+    setLocation(ev.currentTarget.getAttribute('href')!);
+    // console.log(ev.currentTarget.getAttribute('href'))
+  }
+
+  useEffect ( () => {
+    document.title = location;
+    window.history.pushState(location, 'Title', '/' + location)
+  }, 
+  [location]
+  );
+
+
   return (
     <main>
-      <Accueil></Accueil>
+      <nav>
+        {pages.map( (page, i) => ( <a href={page} className={location === page ? "active" : ""} key={i} onClick={handleNavClick}>{page}</a>))}
+      </nav>
+      {location === "Accueil" && (<Accueil />) }
+      {location === "ComposantExample" && (<ComposantExample />) }      
+      <ComposantExample />
     </main>
+    
   );
 }
 
---
+++ src/components/404.tsx
@@ -0,0 +1,10 @@
+const Page404 = () => {
+  return (
+    <section className="page404">
+      <h2>Erreur 404 !</h2>
+      <p>La page demandée n'existe pas.</p>
+    </section>
+  );
+};
+
+export default Page404;
