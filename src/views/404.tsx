6ef4ab72b743b6d5f97c8ffa7d75207427377759
Francois Cepegra <titeca.f@gmail.com>
2022-10-04 15:43:45 +0200

    test sur ecran jour

--- src/views/404.tsx
+++ src/views/404.tsx
@@ -3,6 +3,7 @@ const Page404 = () => {
     <main className="page404">
       <h2>These are not the droids you're looking for 👌 </h2>
       <p>La page demandée n'existe pas.</p>
+      <button className="btn btn-wide">koukou</button>
     </main>
   );
 };
