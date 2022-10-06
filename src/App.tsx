2cf984b29d99a3fbcbd35714b061dcca377bc499
Alexandre Chevalier <alextechnocite@gmail.com>
2022-10-06 15:46:16 +0200

    La demo AR avec les supers Ballekes qui fontionnent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
 import Sliderpersonnage from "./components/Sliderpersonnage";
 import Sliderbackground from "./components/Sliderbackground";
+import Artest from "./components/Artest";
+import { Canvas } from '@react-three/fiber'
+import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import BallPit from "./components/BallPit";
+
 
 
 function App() {
   return (
+    
     <main>
-      
-      <Sliderpersonnage></Sliderpersonnage>
-      <Sliderbackground></Sliderbackground>
+ <>
+      <ARButton sessionInit={{ optionalFeatures: ['local-floor', 'bounded-floor', 'hand-tracking', 'layers'] }}/>
+      <Canvas>
+        <XR>
+          <Controllers />
+          <Hands />
+          <mesh>
+            <BallPit></BallPit>
+            <meshBasicMaterial color="blue" />
+          </mesh>
+        </XR>
+      </Canvas>
+    </>
 
     </main>
   );
