ef0caee13b29d1c73a94fc0a2267c46b0fd72ed6
Alexandre Chevalier <alextechnocite@gmail.com>
2022-10-06 16:26:26 +0200

    Ajout du modele 3D dans la secène

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import Sliderpersonnage from "./components/Sliderpersonnage";
 import Sliderbackground from "./components/Sliderbackground";
 import Artest from "./components/Artest";
 import { Canvas } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
 import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
 import BallPit from "./components/BallPit";
 
-
+function LoadGLB() {
+  const gltf = useGLTF('./public/assets/character/bestdubest.glb', true)
+  return <primitive object={gltf.scene} />
+}
 
 function App() {
   return (
@@ -18,8 +22,12 @@ function App() {
         <XR>
           <Controllers />
           <Hands />
+          <mesh position={[0, 0, 0]} scale={0.008}>
+            <LoadGLB />
+            <meshBasicMaterial color="blue" />
+          </mesh>
           <mesh>
-            <BallPit></BallPit>
+            {/* <BallPit></BallPit> */}
             <meshBasicMaterial color="blue" />
           </mesh>
         </XR>
