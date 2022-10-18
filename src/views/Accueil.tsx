a32ba4f02a2c91e280634934bdd61081436bdb71
Francois Cepegra <titeca.f@gmail.com>
2022-10-18 11:23:04 +0200

    fixed homepage colors & app name

--- src/views/Accueil.tsx
+++ src/views/Accueil.tsx
@@ -75,7 +75,7 @@ const Accueil = ({handleRedirect}) => {
       <button className="btn btn-block py-2 rounded-none btn-primary">
         <i className="fa-sharp fa-solid fa-download px-4"></i> Installer l'application
       </button>
-      <h1 className="text-2xl font-bold text-center">je suis un logoooo lol</h1>
+      <h1 className="text-2xl font-bold text-center">MetaMorpho</h1>
       <section className="flex flex-col my-0 mx-auto w-4/5">
         <button className="btn my-4">Scanner QR Code</button> {/* non fonctionnel pour le moment*/}
         <div className="flex flex-col w-full border-opacity-50">
@@ -97,8 +97,7 @@ const Accueil = ({handleRedirect}) => {
         </form>
       </section>
       <footer className="flex justify-around py-6">
-        <img src="./assets/logo-cepegra.png" alt="Logo du Cepegra" width="50" className="w-3/12" />
-        <img src="./assets/logo-forem.png" alt="Logo du Forem" className="w-3/12" />
+        <img src="./assets/img/logo_cepegra_couleur.svg" alt="Logo du Cepegra" width="50" className="w-3/12" />
       </footer>
     </main>
   );
