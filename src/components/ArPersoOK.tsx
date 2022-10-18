fb1cb851738a407a73b1d16a7d20c7e0d74046fb
Francois Cepegra <titeca.f@gmail.com>
2022-10-18 13:22:01 +0200

    fixed the path to character-fake.glb

--- src/components/ArPersoOK.tsx
+++ src/components/ArPersoOK.tsx
@@ -27,7 +27,7 @@ const ArpersoOK = () => {
 
   return (
     <div className="App">
-    <model-viewer src="./assets/characters/character-fake.glb"
+    <model-viewer src="./assets/character/character-fake.glb"
     poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
     alt="A 3D model of an astronaut"
     shadow-intensity="1"
