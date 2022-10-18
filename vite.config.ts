af35ab7d70eeb116b5c338e82413419750372dfa
Francois Cepegra <titeca.f@gmail.com>
2022-10-18 09:13:27 +0200

    PWA en cours

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({registerType: 'autoUpdate', workbox: {
+    globPatterns: ['**/*.{ts, tsx, js,css,html,ico,png,svg}']
+  }, devOptions: {enabled: true}, injectRegister: null}, )],
   base: "",
   assetsInclude: ['**/*.glb']
 })
